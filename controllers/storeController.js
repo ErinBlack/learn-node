84d18ad5f6de45448eb3e2ed30d83ee070b447aa
erinblack <erinblackdesign@gmail.com>
2019-01-03 08:12:54 -0600

    updated and hooked up google maps API

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,12 +14,28 @@ exports.addStore = (req, res) => {
 } 
 
 exports.createStore = async (req, res) => {
-  const store = await (new Store(req.body)).save();
+  const store = await new Store(req.body).save();
   req.flash('success', `Sucessfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
 
 exports.getStores = async(req, res) => {
-  const stores = await(Store.find());
+  const stores = await Store.find();
   res.render( 'stores', {title: 'Stores', stores });
+}
+
+exports.editStore = async(req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async(req, res) => {
+  req.body.location.type = "Point";
+  
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, { 
+    new: true, 
+    runValidators: true 
+  }).exec();
+  req.flash('success', `Sucessfully updated ${store.name} <a href="/stores/${store.slug}">ViewStore</a>`);
+  res.redirect(`/stores/${store.id}/edit`);
 }
